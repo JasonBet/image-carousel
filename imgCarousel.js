f697ee47389c0c573d3ff93223b84656e4996978
Jason <jasonbetsargon@gmail.com>
2024-12-26 10:05:05 -0800

    Create navagation dots that can switch to specific slide

--- imgCarousel.js
+++ imgCarousel.js
@@ -15,6 +15,27 @@ rightArrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 
 
 const divDots = document.createElement("div");
 divDots.classList.add("div-dots");
+const dot1 = document.createElement("div");
+dot1.classList.add("dot");
+dot1.dataset.index = 0;
+const dot2 = document.createElement("div");
+dot2.classList.add("dot");
+dot2.dataset.index = 1;
+const dot3 = document.createElement("div");
+dot3.classList.add("dot");
+dot3.dataset.index = 2;
+const dot4 = document.createElement("div");
+dot4.classList.add("dot");
+dot4.dataset.index = 3;
+const dot5 = document.createElement("div");
+dot5.classList.add("dot");
+dot5.dataset.index = 4;
+
+divDots.appendChild(dot1);
+divDots.appendChild(dot2);
+divDots.appendChild(dot3);
+divDots.appendChild(dot4);
+divDots.appendChild(dot5);
 
 containerDiv.appendChild(leftArrow);
 containerDiv.appendChild(slide);
@@ -51,12 +72,14 @@ slide.appendChild(img5);
 let imageIndex = -1;
 const totalImages = 5;
 let images = document.getElementsByTagName("img");
+let dots = document.getElementsByClassName("dot");
 let slideTimeout;
 let initial = 0;
-showSlide();
-function showSlide() {
+showNextSlide();
+function showNextSlide() {
     if(initial > 0) {
         images[imageIndex].style.display = "none";
+        dots[imageIndex].classList.remove("current-dot");
     }
     if(imageIndex >= (totalImages -1)) {
         imageIndex = 0;
@@ -65,13 +88,16 @@ function showSlide() {
     if(imageIndex === -1) {imageIndex = 4;}
     if(imageIndex === -2) {imageIndex = 3;}
     images[imageIndex].style.display = "block";
+    dots[imageIndex].classList.add("current-dot");
+
     initial++;
-    slideTimeout = setTimeout(showSlide, 5000);
+    slideTimeout = setTimeout(showNextSlide, 5000);
 }
 
 function showPrevSlide() {
     if(initial > 0) {
         images[imageIndex].style.display = "none";
+        dots[imageIndex].classList.remove("current-dot");
     }
     
     imageIndex--;
@@ -79,16 +105,35 @@ function showPrevSlide() {
     if(imageIndex === -1) {imageIndex = 4;}
     if(imageIndex === -2) {imageIndex = 3;}
     images[imageIndex].style.display = "block";
+    dots[imageIndex].classList.add("current-dot");
     initial++;
-    slideTimeout = setTimeout(showSlide, 5000);
+    slideTimeout = setTimeout(showNextSlide, 5000);
 }
 
 rightArrow.addEventListener("click", () => {
     clearTimeout(slideTimeout);
-    showSlide();
+    showNextSlide();
 });
 
 leftArrow.addEventListener("click", () => {
     clearTimeout(slideTimeout);
     showPrevSlide();
+});
+
+function showSlide(n) {
+    images[imageIndex].style.display = "none";
+    dots[imageIndex].classList.remove("current-dot");
+
+    imageIndex = n;
+    images[imageIndex].style.display = "block";
+    dots[imageIndex].classList.add("current-dot");
+    initial++;
+    slideTimeout = setTimeout(showNextSlide, 5000);
+}
+
+divDots.addEventListener("click", (e) => {
+    if(e.target.dataset.index) {
+        clearTimeout(slideTimeout);
+        showSlide(e.target.dataset.index);
+    }
 });
